e47d93fd35bca268e651e216116c9bc69a0e97bd
Jose Chavez <josmchavz@gmail.com>
2024-04-20 06:08:01 -0700

    removed button

--- App.tsx
+++ App.tsx
@@ -7,10 +7,6 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <Button title="GAH   " onPress={async () => {
-          console.log('Click me')
-        await new Users().createUser('josmchavz@gmail.com', 'test123?!');
-      }} />
     </View>
   );
 }
