5e504505282cd5ddbd200fa65b4f2950ad3c154c
Jose Chavez <josmchavz@gmail.com>
2024-04-20 06:06:59 -0700

    init backend

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {Users} from "./backend/api/users";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="GAH   " onPress={async () => {
+          console.log('Click me')
+        await new Users().createUser('josmchavz@gmail.com', 'test123?!');
+      }} />
     </View>
   );
 }
