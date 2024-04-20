6d0b9db5c18ea5d857a178d84532a5ed32f8744a
rali7196 <97213257+rali7196@users.noreply.github.com>
2024-04-19 21:26:19 -0700

    got react native paper workgin

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+// import { MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
+import Test from './src/test'
+import { AppRegistry, View } from 'react-native';
+import { MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
+import {name as appName} from './app.json'
+import { Button } from 'react-native-paper';
+
+
+// const theme = {
+//   ...DefaultTheme,
+//   colors: {
+//     ...DefaultTheme.colors,
+//     primary: 'tomato',
+//     secondary: 'yellow',
+//   },
+// };
+
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <View 
+      style={{justifyContent:'center', flex:1}} 
+      >
+        <Test />
+        <Button icon="camera" mode="contained">
+          Press Me!
+        </Button>
+        
+
+      </View>
+    </PaperProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+AppRegistry.registerComponent(appName, () => App);
+
