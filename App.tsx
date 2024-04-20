2d78fe9a7870a395a0b894e5795b9e926a01ec44
Harshita B <honeyblue2604@gmail.com>
2024-04-20 12:21:09 -0700

    added line to register the app

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 // import { MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
-import Test from './src/test'
 import { AppRegistry, View } from 'react-native';
 import { MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
 import { Button } from 'react-native-paper';
@@ -28,3 +27,4 @@ export default function App() {
     
 }
 
+AppRegistry.registerComponent("FloraFit", () => App);
