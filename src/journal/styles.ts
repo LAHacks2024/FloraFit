93157482a2851feb34cae455af76da0172b71af8
Jose Chavez <josmchavz@gmail.com>
2024-04-21 05:01:30 -0700

    able to view the journal entry

--- src/journal/styles.ts
+++ src/journal/styles.ts
@@ -6,11 +6,12 @@ export const styles = StyleSheet.create({
     borderRightWidth: 2,
     borderBottomWidth: 2,
     borderBottomColor: '#759CB8',
+    width: '20%',
   },
   right: {
     borderBottomWidth: 2,
     borderBottomColor: '#759CB8',
-    flex: 1,
+    width: '80%',
   },
   header: {
     fontSize: 40,
@@ -28,5 +29,22 @@ export const styles = StyleSheet.create({
     paddingHorizontal: 9.2,
     paddingVertical: 3,
     fontStyle: "italic",
+  },
+  modalContainer: {
+    backgroundColor: '#fff',
+    padding: 25,
+    margin: 20,
+    minHeight: 550,
+    justifyContent: 'flex-start'
+  },
+  modalHeader: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#000',
+    marginBottom: 20,
+  },
+  modalText: {
+    fontSize: 20,
+    color: '#000',
   }
 });
