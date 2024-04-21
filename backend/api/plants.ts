edc8e31ab462bbc371c78ac6dd91ab10a72aab76
rali7196 <97213257+rali7196@users.noreply.github.com>
2024-04-20 22:31:37 -0700

    pulling on main

--- backend/api/plants.ts
+++ backend/api/plants.ts
@@ -1,6 +1,6 @@
 import {Plant, PlantDTO} from "../entities/plant.model";
 import {FloraFirebase} from "./FloraFirebase";
-
+//test
 export class Plants extends FloraFirebase<Plant, PlantDTO> {
     constructor() {
         super('plants');
