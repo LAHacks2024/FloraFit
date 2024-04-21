aef43480bb9f15e252b97d58f9f3b21b365b9c25
audsostrom <audreyostrom64@gmail.com>
2024-04-20 17:35:24 -0700

    added step counting

---
+++ src/landing/style.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    map: {
+      flex: 1,
+      width: '100%'
+    },
+    login_and_signup_button: {
+      borderRadius: 10,
+      backgroundColor: '#2A3779',
+      padding:10
+    },
+    button_text: {
+      fontWeight: 'bold',
+      fontFamily: 'DMSans',
+      alignContent: 'center',
+      color:'white'
+    }
+
+  });
