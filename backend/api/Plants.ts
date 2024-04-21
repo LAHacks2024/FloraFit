84754cfe920749237b9862f35333d77ab1f48fa3
audsostrom <98059264+audsostrom@users.noreply.github.com>
2024-04-21 02:25:09 -0700

    Delete backend/api/Plants.ts

--- backend/api/Plants.ts
+++
@@ -1,8 +0,0 @@
-import {Plant, PlantDTO} from "../entities/plant.model";
-import {FloraFirebase} from "./FloraFirebase";
-
-export class PlantsDB extends FloraFirebase<Plant, PlantDTO> {
-    constructor() {
-        super('plants');
-    };
-}
